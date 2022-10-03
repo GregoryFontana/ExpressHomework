4532b226786e949a88705bfba804b1b8c9261c01
GregoryFontana <gregjfontana@gmail.com>
2022-10-03 01:50:31 -0400

    Magic 8 Ball is not quite functional

--- server.js
+++ server.js
@@ -6,8 +6,14 @@ const app = express()
 //     res.send('<h1>Hello, Gregory! It is so great to see you!</h1>');
 // });
 
-app.get('/tip/:total/:tipPercentage', (req, res)=> {const {total, tipPercentage} = req.params
-    res.send(`Your tip will be ${tipPercentage}`);
+// app.get('/tip/:total/:tipPercentage', (req, res)=> {const {total, tipPercentage} = req.params
+//     res.send(`Your tip will be ${tipPercentage}`);
+// });
+
+const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+console.log(eightBall)
+app.get('/magic/:Will%20I%20Be%20A%20Millionaire', (req, res)=> {const {WillIBeAMillionaire} = req.params
+    res.send(`<h1>Will I be a Millionaire? Answer ${eightBall[Math.floor(Math.random()*20)]}</h1>`);
 });
 
 
