ef2eac5db17c9e2fbd27129610204d3446a63eb8
GregoryFontana <gregjfontana@gmail.com>
2022-10-03 01:31:24 -0400

    2nd Commit

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const app = express()
 
-app.get('/greeting/:name', function(req, res) {
-    res.send('<h1>Hello, Gregory! It is so great to see you!</h1>');
+// app.get('/greeting/:name', function(req, res) {
+//     res.send('<h1>Hello, Gregory! It is so great to see you!</h1>');
+// });
+
+app.get('/tip/:total/:tipPercentage', (req, res)=> {const {total, tipPercentage} = req.params
+    res.send(`Your tip will be ${tipPercentage}`);
 });
 
 
